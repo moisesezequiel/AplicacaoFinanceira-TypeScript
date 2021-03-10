174c6b5896e400c30f0fe372dfc4e1ef44fc849c
moisesezequiel <contato.moisesezequiel@outlook.com>
2021-03-10 02:49:04 -0300

    incluindo algumas linhas de JQuery nas funções

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,8 +1,8 @@
 class View {
     constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor); //seletor Jquery
     }
     update(model) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,15 @@
+declare var $:any;//declarando o $() do Jquery
+
 abstract class View <T>{
     //definindo classe abstrata para definir que nao poderá ser criado uma instancia
-    protected _elemento:Element;
+    protected _elemento:any; //mudando de Element para Any para caber dentro do metodo update
 
     constructor(seletor:string){//selecionando o id que está la no html
-        this._elemento = document.querySelector(seletor)
+        this._elemento = $(seletor);//seletor Jquery
     }
 
-    update(model:T) :void{
-        this._elemento.innerHTML = this.template(model);
+    update(model:T) {
+        this._elemento.html(this.template(model));
     }
 
    abstract template(model:T) : string;//metodo abstrato para ser implementado nas classes filhas 
